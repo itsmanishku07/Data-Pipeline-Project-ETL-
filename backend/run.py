483e43ba3ce488bee68f521f5511d7408c8e7e4b
itsmanishku07 <manish0704ku@gmail.com>
2026-08-25 01:43:36 +0530

    Example Env Added

--- backend/run.py
+++ backend/run.py
@@ -10,7 +10,6 @@ if sys.platform == "win32":
         pass
 
 if __name__ == "__main__":
-    # Ensure current directory is on python path
     sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
     print("[INFO] Starting DataFlow Studio FastAPI backend server on http://localhost:8000 ...")
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
