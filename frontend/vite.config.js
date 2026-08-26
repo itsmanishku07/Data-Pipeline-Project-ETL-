feff0f7dace82fef31fc10a3cc9af0210c794a4e
Manish Kumar <76082844+itsmanishku07@users.noreply.github.com>
2026-08-26 15:59:57 +0530

    Set host to '0.0.0.0' for server and preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,12 +19,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
     port: 3000,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
@@ -34,6 +34,7 @@ export default defineConfig({
   },
 
   preview: {
+    host: '0.0.0.0',
     allowedHosts: true,
   }
 })
