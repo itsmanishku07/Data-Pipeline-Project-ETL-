8fe971ed1e0e587d2210f2ebc9899c40bfffa9ea
Manish Kumar <76082844+itsmanishku07@users.noreply.github.com>
2026-08-27 11:37:02 +0530

    Update Vite config to change API target address

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,22 +19,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log('===== VITE CONFIG LOADED =====')
-
 export default defineConfig({
   plugins: [react()],
-
   preview: {
     host: '0.0.0.0',
     allowedHosts: true,
   },
-
   server: {
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://20.219.71.141:8000',
         changeOrigin: true,
       }
     }
